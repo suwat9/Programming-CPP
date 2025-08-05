7441341d88756a4b09b72baffd580dc25b8f7d43
Suwat <suwat.tac@mail.pbru.ac.th>
2025-08-05 11:20:13 +0700

    Update Lab25-2.cpp

--- Chapter25/Lab25-2.cpp
+++ Chapter25/Lab25-2.cpp
@@ -1,1 +1,36 @@
-xxx
+#include <iostream>
+#include <cmath>
+
+class Base_class {
+	public:
+	int base, height;
+	int width, length;
+	double radius;
+	
+	void findArea() {
+		std::cout << "findArea() in Base class" << std::endl;
+	}
+	void perimeter() {
+		std::cout << "perimeter() in Base class" << std::endl;
+	}
+};
+class Triangle : public Base_class {
+	public:
+		void findArea() {
+			std::cout << "Area of Triangle is " << (0.5 * base * height) << std::endl;
+		}
+		void perimeter() {
+			double c= std::sqrt((base*base) + (height*height));
+			std::cout << "Perimeter of triangle is " << (base + height + c) << std::endl;
+		}
+};
+class Rectangle : public Base_class {
+	
+};
+class Circle : public Base_class {
+	
+};
+
+int main() {
+	
+}
