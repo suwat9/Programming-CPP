44c1b59918884161543373fad49709c3879411a1
Suwat <suwat.tac@mail.pbru.ac.th>
2025-09-02 11:14:50 +0700

    Add files via upload

---
+++ Lab4-Chapter1-4/Chapter3-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {4, 2, 5, 3, 5, 8, 3};
+    sort(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
